e7d15e4d1637e95816b4aba0e6293b93dff0a72b
Eloi Magnien <eloi.magnien@gmail.com>
2022-02-23 11:57:18 +0100

    delete unused code

--- src/pages/Stats/Stats.jsx
+++ src/pages/Stats/Stats.jsx
@@ -1,62 +1,11 @@
 import './Stats.scss'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import Button from '../../components/Button/Button';
-import { useEffect, useState } from 'react';
-import { getLocalStorage, removeLocalStorage } from '../../utils/localStorage';
-import Fireworks from '../../components/Fireworks/Fireworks';
+import { useSelector } from 'react-redux';
+
 
 const Stats = () => {
-  const {id} = useParams();
-  const state = useSelector((state) => state)
-  const [localStorageAvailable, setIsLocalStorageAvailable] = useState(true)
-  const dispatch = useDispatch()
-  
-    /**
-     * Check availability to use localStorage
-     */
-     const isLocalStorageAvailable = () => {
-      const test = 'test'
-      try {
-        localStorage.setItem(test, test)
-        localStorage.removeItem(test)
-        setIsLocalStorageAvailable(true)
-      } catch (e) {
-        setIsLocalStorageAvailable(false)
-      }
-    }
-  
-    useEffect(() => {
-      isLocalStorageAvailable()
-      const localStorage = JSON.parse(getLocalStorage())
-      if (localStorageAvailable && localStorage && localStorage.state.teams.length === state.teams.length) {
-        dispatch({ type: "setStateWinner"})
-      }
-    }, [dispatch, localStorageAvailable, state.teams.length])
-
-    const handleRestartGame = () => {
-      for (let i = 0; i < state.teams.length; i++) {
-        dispatch({type: "restart", idx: i})
-      }
-      // check localStorage
-      const rawLocalStorage = getLocalStorage()
-      // si il y a quelqueChose dans le localStorage
-      if (rawLocalStorage !== null) {
-          removeLocalStorage()
-        }
-      }
-
-    const handleStartNewGame = () => {
-      dispatch({type: "startNewGame"})
-      // check localStorage
-      const rawLocalStorage = getLocalStorage()
-      // si il y a quelqueChose dans le localStorage
-      if (rawLocalStorage !== null) {
-          removeLocalStorage()
-        }
-      }
+const state = useSelector((state) => state)
 
 return (
   <div id="Stats" className="Stats">
